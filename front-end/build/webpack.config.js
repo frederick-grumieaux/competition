a3b8d9d10bc14348a0f076ed15498e8c3bb18e27
Frederick Grumieaux <grumieaux.f@gmail.com>
2018-11-18 17:41:40 +0100

    + include sass conversion to css in compilation

--- front-end/build/webpack.config.js
+++ front-end/build/webpack.config.js
@@ -1,17 +1,30 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "none", //Please no auto magic, even if it is good for me. 
-    entry: path.resolve(__dirname, "../src/index.js"),
+    entry: {
+        app: path.resolve(__dirname, "../src/index.js"),
+        styles: path.resolve(__dirname, "../src/styles/style.scss")
+    },
     output: {
         path: path.resolve(__dirname, "../dist"),
-        filename: 'app.js',
+        //filename: 'app.js',
     },
     plugins: [
         //Copy all the files from the ../www/**/* folder to the output folder (= ../dist)
-        new CopyWebpackPlugin([{ from: 'www', force: true }], { context: '../.' })
+        new CopyWebpackPlugin([{ from: 'www', force: true }], { context: '../.' }),
+        new MiniCssExtractPlugin({ filename: "[name].css", chunkFilename: "[id].css" })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ],
+    },
     devServer: {
         contentBase: path.join(__dirname, '../dist'),
         compress: true,
